6137fa87b63d03d92c60adca93593bde35f3ec07
goraedoll21 <goraedoll21@gmail.com>
2024-12-02 15:15:29 +0900

    App page 제작

---
+++ app/ui.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Logo from "components/logo";
+import SearchComponent from "components/search-component";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function UI() {
+  const [searchInput, setSearchInput] = useState("");
+
+  return (
+    <main className="flex w-full p-2 flex-col gap-4">
+      <Logo />
+      <SearchComponent />
+    </main>
+  );
+}
